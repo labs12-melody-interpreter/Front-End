bd606f6649eceaf21e70bfbd9e30904155264efd
Blake Fletcher <blkfltchr@gmail.com>
2019-05-10 14:07:58 -0400

    Play function

--- src/components/ToneJS/index.js
+++ src/components/ToneJS/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import  Midi  from "@tonejs/midi"
-import Tone from "tone"
-
+import { } from "@tonejs/ui"
+import {Tone} from "tone"
 
 function fileDrop() {
   if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
@@ -35,18 +35,19 @@ function parseFile(file){
   reader.onload = function(e){
     const midi = new Midi(e.target.result)
     document.querySelector("#ResultsText").value = JSON.stringify(midi, undefined, 2)
-    //document.querySelector('#tone-play-toggle').removeAttribute('disabled')
+    document.querySelector('#tone-play-toggle').removeAttribute('disabled')
     currentMidi = midi
   }
   reader.readAsArrayBuffer(file)
 }
+
 function play(){
   console.log("currentMidi",currentMidi)
   const synths = []
   document.querySelector('tone-play-toggle').addEventListener('play', (e) => {
     const playing = e.detail
     if (playing && currentMidi){
-      const now = Tone.now() + 0.5
+      // const now = new Tone.now() + 0.5
       currentMidi.tracks.forEach(track => {
         //create a synth for each track
         const synth = new Tone.PolySynth(10, Tone.Synth, {
@@ -60,7 +61,7 @@ function play(){
         synths.push(synth)
         //schedule all of the events
         track.notes.forEach(note => {
-          synth.triggerAttackRelease(note.name, note.duration, note.time + now, note.velocity)
+          synth.triggerAttackRelease(note.name, note.duration, note.time, note.velocity)
         })
       })
     } else {
@@ -72,16 +73,14 @@ function play(){
     }
   })
 }
+
 const ToneJS = () => {
   
   
     return (
         <div>
-          <title>Tonejs Midi</title>
           <style type="text/css" dangerouslySetInnerHTML={{__html: "\n\t\t#FileDrop{\n\t\t\tposition: relative;\n\t\t\twidth: 100%;\n\t\t\theight: 100px;\n\t\t\tborder: 2px dashed black;\n\t\t\tcolor: black;\n\t\t\tmargin: 20px auto;\n\t\t}\n\t\t#FileDrop.Hover{\n\t\t\tbackground-color: black;\n\t\t\tcolor: white;\n\t\t}\n\t\t#FileDrop input {\n\t\t\tposition: absolute;\n\t\t\twidth: 100%;\n\t\t\theight: 100%;\n\t\t\topacity: 0;\n\t\t\tleft: 0px;\n\t\t\ttop: 0px;\n\t\t}\n\t\t#FileDrop #Text {\n\t\t\tposition: absolute;\n\t\t\twidth: 100%;\n\t\t\theight: 20px;\n\t\t\tline-height: 20px;\n\t\t\tleft: 0px;\n\t\t\ttop: 50%;\n\t\t\ttransform: translate(0, -50%);\n\t\t\ttext-align: center;\n\t\t}\n\t\ttextarea {\n\t\t\tfont-family: monospace;\n\t\t\theight: 300px;\n\t\t\twidth: 100%;\n\t\t\tdisplay: inline-block;\n\t\t\tposition: relative;\n\t\t\tfloat: left;\n\t\t}\n\t\t#Results {\n\t\t\tposition: relative;\n\t\t\twidth: 100%;\n\t\t\tmargin: 10px auto;\n\t\t}\n\t\t#Description {\n\t\t\tposition: relative;\n\t\t\twidth: 100%;\n\t\t\ttext-align: center;\n\t\t\theight: 40px;\n\t\t\tfont-size: 16px;\n\t\t\tmargin: 10px auto;\n\t\t\tfont-family: sans-serif;\n\t\t}\n\t\ttone-play-toggle {\n\t\t\tmargin-top: 10px;\n\t\t}\n\t" }} />
-          <tone-top-bar />
           <tone-content>
-            <div id="Description">Parse a MIDI file into a Tone.js-friendly JSON format.</div>
             <div id="FileDrop">
               <div id="Text">
                 Drop a midi file here
@@ -92,7 +91,6 @@ const ToneJS = () => {
               <textarea id="ResultsText" placeholder="json output..." defaultValue={""} />
             </div>
             <tone-play-toggle disabled id="tone-play-toggle" onClick={ play }/> 
-            
           </tone-content>
         </div>
       );
