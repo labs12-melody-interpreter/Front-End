6589f9c8df7a61e41df0d9c6dee7063f2ee4f712
Blake Fletcher <blkfltchr@gmail.com>
2019-05-14 16:24:45 -0400

    Play and animate working, but timing is off

--- src/components/MusicLib/PlayPause.js
+++ src/components/MusicLib/PlayPause.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react'
 import { Motion, spring } from 'react-motion'
 
 class PlayPause extends Component {
-  onClickPlay = event => {
-    this.props.onClick()
+
+  constructor(props) {
+    super(props)
+    this.play = this.play.bind(this);
+  }
+
+  async animate() {
+    console.log("animate props", this.props)
+    this.props.animate()
+  }
+
+  async play() {
+    console.log("play this", this)
     this.props.play()
+  }
+
+  onClickPlay = event => {
+    this.animate().then(this.play)
   }
   render() {
     const { toggle } = this.props
     
     return(
       <Motion
-        style={{scale: spring(toggle ? 1 : 0, [300, 30])}}
+        style={{scale: spring(toggle ? 1 : 0, [1000, 30])}}
       >
         {({scale}) =>
           <button
