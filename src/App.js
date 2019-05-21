1b5e8b3fb32398fe533e8dadaf2ea728b8d08c88
Tony Tia <webmaster@tonytia.com>
2019-05-21 12:14:18 -0700

    updated route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react"
 import { Route, NavLink } from 'react-router-dom';
 import MidiPiano from "./components/MidiPiano"
-import ToneJS from "./components/ToneJS"
 import UserInput from "./components/UserInput";
+import MelodyApp from "./components/MelodyApp"
 
 
 class App extends Component {
@@ -15,7 +15,7 @@ class App extends Component {
         <NavLink style={{margin: "10px"}} to="/library">Library</NavLink>
         <NavLink style={{margin: "10px"}} to="/generator">Generator</NavLink>
         <Route path='/piano' component={MidiPiano}/>
-        <Route path='/library' component={ToneJS} />
+        <Route path='/library' component={MelodyApp} />
         <Route path='/generator' component={UserInput} />
       </div>
     );
