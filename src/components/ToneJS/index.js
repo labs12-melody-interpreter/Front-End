d0d3712f833b0bf3c16f59e48e3d8773c7bd6a6c
Tony Tia <webmaster@tonytia.com>
2019-05-22 17:03:26 -0700

    broken button

--- src/components/ToneJS/index.js
+++ src/components/ToneJS/index.js
@@ -8,7 +8,7 @@ function fileDrop() {
   if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
     document.querySelector("#FileDrop #Text").textContent = "Reading files not supported by this browser";
   } else {
-
+/*
     const fileDrop = document.querySelector("#FileDrop")
 
     fileDrop.addEventListener("dragenter", () => fileDrop.classList.add("Hover"))
@@ -16,13 +16,18 @@ function fileDrop() {
     fileDrop.addEventListener("dragleave", () => fileDrop.classList.remove("Hover"))
 
     fileDrop.addEventListener("drop", () => fileDrop.classList.remove("Hover"))
+*/
+    //this.setState(button => ({ isToggle: !button.isToggle}));
+    //const enabledButton = this.button.isToggle;
 
     document.querySelector("#FileDrop input").addEventListener("change", e => {
       //get the files
       const files = e.target.files
       if (files.length > 0){
         const file = files[0]
-        document.querySelector("#FileDrop #Text").textContent = file.name
+        var msg = "Hit  Play  below to play := " + file.name
+        document.querySelector("#FileDrop #Text").textContent = msg
+        //document.querySelector('playStop').setAttribute('enabled')
         parseFile(file)
       }
     })
@@ -36,7 +41,7 @@ function parseFile(file){
   reader.onload = function(e){
     const midi = new Midi(e.target.result)
     currentMidi = midi
-    //document.querySelector('Button').removeAttribute('disabled')
+    
   }
   reader.readAsArrayBuffer(file)
 }
@@ -49,6 +54,7 @@ class ToneJS extends Component {
     this.state = { isToggle : true }
     this.handleClick = this.handleClick.bind(this);
     this.synth = null;
+    this.button = { isToggle : false };
   }
   handleClick() {
     this.setState(state => ({ isToggle: !state.isToggle}));
@@ -78,6 +84,7 @@ class ToneJS extends Component {
     }
   } 
   render() {
+
     return (
         <div>
 <style type="text/css" dangerouslySetInnerHTML={{__html: "\n\t\t#FileDrop{\n\t\t\tposition: relative;\n\t\t\twidth: 100%;\n\t\t\theight: 100px;\n\t\t\tborder: 2px dashed black;\n\t\t\tcolor: black;\n\t\t\tmargin: 20px auto;\n\t\t}\n\t\t#FileDrop.Hover{\n\t\t\tbackground-color: black;\n\t\t\tcolor: white;\n\t\t}\n\t\t#FileDrop input {\n\t\t\tposition: absolute;\n\t\t\twidth: 100%;\n\t\t\theight: 100%;\n\t\t\topacity: 0;\n\t\t\tleft: 0px;\n\t\t\ttop: 0px;\n\t\t}\n\t\t#FileDrop #Text {\n\t\t\tposition: absolute;\n\t\t\twidth: 100%;\n\t\t\theight: 20px;\n\t\t\tline-height: 20px;\n\t\t\tleft: 0px;\n\t\t\ttop: 50%;\n\t\t\ttransform: translate(0, -50%);\n\t\t\ttext-align: center;\n\t\t}\n\t\ttextarea {\n\t\t\tfont-family: monospace;\n\t\t\theight: 300px;\n\t\t\twidth: 100%;\n\t\t\tdisplay: inline-block;\n\t\t\tposition: relative;\n\t\t\tfloat: left;\n\t\t}\n\t\t#Results {\n\t\t\tposition: relative;\n\t\t\twidth: 100%;\n\t\t\tmargin: 10px auto;\n\t\t}\n\t\t#Description {\n\t\t\tposition: relative;\n\t\t\twidth: 100%;\n\t\t\ttext-align: center;\n\t\t\theight: 40px;\n\t\t\tfont-size: 16px;\n\t\t\tmargin: 10px auto;\n\t\t\tfont-family: sans-serif;\n\t\t}\n\t\ttone-play-toggle {\n\t\t\tmargin-top: 10px;\n\t\t}\n\t" }} /> 
@@ -88,10 +95,12 @@ class ToneJS extends Component {
                 Click here to load a midi file
               </div>
               <input type="file" accept="audio/midi"  onClick={fileDrop}/>
+          
             </div>
             
             
-            <Button id='playStop'  variant="contained" color="primary" onClick={() => this.handleClick()}>
+            <Button id='playStop'  disabled={enabledButton}
+            variant="contained" color="primary" onClick={() => this.handleClick()}>
             {this.state.isToggle ? 'Play' :'Stop' }
         </Button>
          
