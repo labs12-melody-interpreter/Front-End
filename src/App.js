9d7772cd05fb72b742a31e928d93c8f5213c0404
Josh Solis <joshsolis@joshs-mbp.attlocal.net>
2019-05-23 13:31:03 -0500

    added notes dropdown

--- src/App.js
+++ src/App.js
@@ -46,13 +46,13 @@ class App extends Component {
                 </IconButton>
                 <Menu id="render-props-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>          
                   <MenuItem onClick={handleClose}>
+                    <NavLink style={{margin: "10px", textDecoration:'none', color:'unset'}} to="/">Home</NavLink>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
                     <NavLink style={{margin: "10px", textDecoration:'none', color:'unset'}} to="/generator/">Generator</NavLink>
                   </MenuItem>
                   <MenuItem onClick={handleClose}>
                     <NavLink style={{margin: "10px", textDecoration:'none', color:'unset'}} to="/library">Library</NavLink>
-                  </MenuItem>
-                  <MenuItem onClick={handleClose}>
-                    <NavLink style={{margin: "10px", textDecoration:'none', color:'unset'}} to="/">Main Menu</NavLink>
                   </MenuItem>
                 </Menu>
                 
